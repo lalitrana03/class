dd75c697719408a33b74191c6e5e622bdd81f60a
lalitrana03 <lalit.37354@stu.upes.ac.in>
2026-09-16 00:10:59 +0530

    day 20

---
+++ day20_.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char binary[33]; 
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    for (int i = 0; i < strlen(binary); i++) {
+        if (binary[i] == '0')
+            binary[i] = '1';
+        else if (binary[i] == '1')
+            binary[i] = '0';
+        else {
+            printf("Invalid binary digit detected.\n");
+            return 1;
+        }
+    }
+
+    printf("1's Complement = %s\n", binary);
+
+    return 0;
+}
