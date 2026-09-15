02cba7490a47ae40017a874df913935d6489419f
lalitrana03 <lalit.37354@stu.upes.ac.in>
2026-09-16 00:07:08 +0530

    day 16

---
+++ day16_.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Binary = ");
+    for (int i = 31; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        printf("%d", bit);
+    }
+    printf("\n");
+
+    return 0;
+}
